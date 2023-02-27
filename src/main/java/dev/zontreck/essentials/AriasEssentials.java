85280c13c02cdf46603795becec8fc9040dd51c0
Aria <tarapiccari@gmail.com>
2023-02-27 13:46:36 -0700

    Begin setting up new mod

---
+++ src/main/java/dev/zontreck/essentials/AriasEssentials.java
@@ -0,0 +1,10 @@
+package dev.zontreck.essentials;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AriasEssentials.MODID)
+public class AriasEssentials {
+    public static final String MODID = "ariasessentials";
+
+
+}
