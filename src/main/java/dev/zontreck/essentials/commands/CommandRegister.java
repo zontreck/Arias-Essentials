563b99180059cd8a6be162df2f864926a6c503d7
Aria <tarapiccari@gmail.com>
2023-02-27 16:25:25 -0700

    Add fixed commands to the register

--- src/main/java/dev/zontreck/essentials/commands/CommandRegister.java
+++ src/main/java/dev/zontreck/essentials/commands/CommandRegister.java
@@ -1,6 +1,15 @@
 package dev.zontreck.essentials.commands;
 
-import dev.zontreck.otemod.commands.homes.DelHomeCommand;
+import dev.zontreck.essentials.commands.homes.DelHomeCommand;
+import dev.zontreck.essentials.commands.homes.HomeCommand;
+import dev.zontreck.essentials.commands.homes.HomesCommand;
+import dev.zontreck.essentials.commands.homes.SetHomeCommand;
+import dev.zontreck.essentials.commands.teleport.RTPCommand;
+import dev.zontreck.essentials.commands.teleport.TPACommand;
+import dev.zontreck.essentials.commands.teleport.TPAHereCommand;
+import dev.zontreck.essentials.commands.teleport.TPAcceptCommand;
+import dev.zontreck.essentials.commands.teleport.TPCancelCommand;
+import dev.zontreck.essentials.commands.teleport.TPDenyCommand;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -8,6 +17,16 @@ public class CommandRegister {
     @SubscribeEvent
     public void onCommandRegister(final RegisterCommandsEvent ev)
     {
-        DelHomeCommand.register(ev);
+        DelHomeCommand.register(ev.getDispatcher());
+        HomeCommand.register(ev.getDispatcher());
+        HomesCommand.register(ev.getDispatcher());
+        SetHomeCommand.register(ev.getDispatcher());
+
+        RTPCommand.register(ev.getDispatcher());
+        TPAcceptCommand.register(ev.getDispatcher());
+        TPAHereCommand.register(ev.getDispatcher());
+        TPACommand.register(ev.getDispatcher());
+        TPCancelCommand.register(ev.getDispatcher());
+        TPDenyCommand.register(ev.getDispatcher());
     }
 }
